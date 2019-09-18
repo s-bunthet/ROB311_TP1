bc2a575cad96dde1f597da073a7f52a0a25c9ec5
bunthet01 <bunthetsay@gmail.com>
2019-09-18 16:35:00 +0200

    add Manhattan distance and argparse

--- train.py
+++ train.py
@@ -1,38 +1,51 @@
 import numpy as np
 import pandas as pd
-import operator
+import argparse
 
-K_list = [1]
-TESTING_RATIO = 0.1  # 20% of training data for testing
-random_seed = 0
+parser = argparse.ArgumentParser(description='Argument to train KNN')
+parser.add_argument('--seed', type=int, default=0, help='random seed')
+parser.add_argument('--d-mode', type=str, default='Euclide', choices=['Euclide', 'Manhattan'], help='distance used in the algorithm')
+args = parser.parse_args()
+
+assert args.seed > 0, "The random seed must be a positive integer."
+
+K_list = [3, 5, 7]
+TESTING_RATIO = 0.2  # 20% of training data for testing
+
 
 # read the data
 data = pd.read_csv("data/breast-cancer-wisconsin.data").values
 data = np.delete(data, 0, axis=1)   # remove the index in first column
+# TODO[]: find the way to fill  the missing value
 data = np.delete(data, 5, axis=1)   # remove the 5th column, which has incomplete value (in some line, it is filled with '?')
-np.random.seed(random_seed)
+np.random.seed(args.seed)
 np.random.shuffle(data)
 
 data_test = data[np.arange(int(TESTING_RATIO*data.shape[0]))]
 data_train = data[np.arange(int(TESTING_RATIO*data.shape[0]), data.shape[0])]
 num_features = data_train.shape[1]-1
+print('Training size: ', data_train.shape[0])
+print('Testing size: ', data_test.shape[0])
 
 
-def distance(x, y, mode='euclide'):
+def distance(x, y, mode='Euclide'):
     """
     Calculate the distance between two points.
     :param x: (np.array)
     :param y: (np.array)
-    :param mode:
+    :param mode: (str)
     :return:
     """
-    assert mode in ['euclide'], 'choose a right distance formula'
-    if mode == 'euclide':
+    assert mode in ['Euclide','Manhattan'], 'choose a right distance formula'
+    if mode == 'Euclide':
         return np.linalg.norm(x-y)
+    elif mode == 'Manhattan':
+        return np.sum(np.absolute(x-y))
 
 
 def train():
     """
+    Train KNN algorithm.
 
     :return:
     """
@@ -42,11 +55,11 @@ def train():
         for i in range(data_test.shape[0]):
             list_class_dis = []
             for j in range(data_train.shape[0]):
-                d = distance(data_test[i][range(num_features)], data_train[j][range(num_features)])
+                d = distance(data_test[i][range(num_features)], data_train[j][range(num_features)], mode=args.d_mode)
                 list_class_dis.append([data_train[j][-1], d])
             arr_class_dis = np.asarray(list_class_dis)
             predicted_class = np.bincount(arr_class_dis[:, 0][range(k)].astype(int)).argmax()
-            print(predicted_class)
+            # fill the confusion matrix
             if data_test[i][-1] == 2:
                 if predicted_class == data_test[i][-1]:
                     confusion_mat[0, 0] += 1
@@ -62,6 +75,7 @@ def train():
         acc = (confusion_mat[0, 0]+confusion_mat[1, 1])/np.sum(confusion_mat)
         accuracy_dict.update({k: acc})
     print("Dictionary of accuracy: ", accuracy_dict)
+    print("Distance used: ", args.d_mode)
 
 
 if __name__ == "__main__":
